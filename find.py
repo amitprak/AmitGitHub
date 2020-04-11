c086ee9aa37aded55e6ec01b2058ca495aca87a6
amitprak <amit_prakash_majumdar@yahoo.co.in>
2020-04-11 18:03:12 -0400

    Add files via upload

---
+++ find.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+os.chdir('C:/Users/AmitPrakash/Desktop/python code')
+excel_file='example.xlsx'
+df=pd.read_excel(excel_file)
+doc=df['Name'].where(df['Job']=='Doctor')
+print(doc.dropna())
+
+print('Below will show all Lines which match')
+print(df[df['Job']=='Doctor'])
+#total_count=df.groupby(['Job']).count()
+##print(total_count['Name'])
+#print('below is logical statment where job is Engineer then print')
+#print(df.loc[df["Job"]=="Engineer"])
